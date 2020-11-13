5b8d402097e67b23e6744428ed78736c7920df2d
Peter Robinson <peter.robinson@jax.org>
2020-11-13 14:59:50 -0500

    gene set extractor with uniprot

--- src/main/java/org/jax/sbas2go/analysis/GeneSetExtractor.java
+++ src/main/java/org/jax/sbas2go/analysis/GeneSetExtractor.java
@@ -3,6 +3,8 @@ package org.jax.sbas2go.analysis;
 import org.monarchinitiative.phenol.analysis.AssociationContainer;
 import org.monarchinitiative.phenol.analysis.DirectAndIndirectTermAnnotations;
 import org.monarchinitiative.phenol.analysis.PopulationSet;
+import org.monarchinitiative.phenol.analysis.StudySet;
+import org.monarchinitiative.phenol.annotations.formats.go.GoGaf21Annotation;
 import org.monarchinitiative.phenol.ontology.data.Ontology;
 import org.monarchinitiative.phenol.ontology.data.TermId;
 
@@ -20,36 +22,51 @@ public class GeneSetExtractor {
 
     private final Ontology ontology;
 
+
+
+    private final Map<String, TermId> symbol2uniprotMap;
+
     public GeneSetExtractor(Map<String, String> ensembl2symbol,
                             Map<String, TermId> ensembl2termId,Map<String,
                             TermId> symbol2termId,
                             AssociationContainer associationContainer,
+                            List<GoGaf21Annotation> annots,
                             Ontology ontology) {
         this.ensembl2symbolMap = ensembl2symbol;
         this.ensembl2termId = ensembl2termId;
         this.symbol2termIdMap = symbol2termId;
         this.associationContainer = associationContainer;
         this.ontology = ontology;
+        symbol2uniprotMap = new HashMap<>();
+        for (var termAnnot : annots) {
+            TermId uniprotId = termAnnot.getTermId();
+            String symbol = termAnnot.getDbObjectSymbol();
+           symbol2uniprotMap.putIfAbsent(symbol, uniprotId);
+        }
+        System.out.printf("Extracted %d symbol to Uniprot TermId mappings.\n", symbol2termId.size());
     }
 
     /**
      * Get all genes used for either DGE or DAS analysis.
+     * We need to map to Uniprot IDs
      * @param dgePopulation List of ensembl ids for DGEs
      * @param asGeneSymbols List of symbols for DASs
      * @return
      */
     public PopulationSet getPopulationSet(Set<String> dgePopulation, Set<String> asGeneSymbols) {
         Set<TermId> tidSet = new HashSet<>();
+        Set<String> allSymbols = new HashSet<>();
+        allSymbols.addAll(asGeneSymbols);
         for (var ens : dgePopulation) {
-            Optional<TermId> t = Optional.ofNullable(ensembl2termId.get(ens));
-            if (t.isPresent()) {
-                tidSet.add(t.get());
+            Optional<String> symbolOpt = Optional.ofNullable(ensembl2symbolMap.get(ens));
+            if (symbolOpt.isPresent()) {
+                allSymbols.add(symbolOpt.get());
             } else {
                 System.err.println("Could not find ENS-" + ens);
             }
         }
-        for (var symbol : asGeneSymbols) {
-            Optional<TermId> t = Optional.ofNullable(symbol2termIdMap.get(symbol));
+        for (var symbol : allSymbols) {
+            Optional<TermId> t = Optional.ofNullable(symbol2uniprotMap.get(symbol));
             if (t.isPresent()) {
                 tidSet.add(t.get());
             } else {
@@ -58,6 +75,30 @@ public class GeneSetExtractor {
         }
         Map<TermId, DirectAndIndirectTermAnnotations> populationAssociations = associationContainer.getAssociationMap(tidSet, ontology);
         return new PopulationSet(tidSet, populationAssociations);
+    }
+
+
+    public StudySet getStudySet(Set<Dge> dgeSet, String name) {
+        Set<TermId> tidSet = new HashSet<>();
+        int notFound = 0;
+        for (var dge : dgeSet) {
+            String ens = dge.getEnsg();
+            Optional<String> symbolOpt = Optional.ofNullable(ensembl2symbolMap.get(ens));
+            if (symbolOpt.isPresent()) {
+                Optional<TermId> tidOpt = Optional.ofNullable(symbol2uniprotMap.get(symbolOpt.get()));
+                if (tidOpt.isPresent()) {
+                    tidSet.add(tidOpt.get());
+                } else {
+                    notFound++;
+                }
+            } else {
+                notFound++;
+            }
+        }
+        System.out.printf("[INFO] Found %d TermIds, could not find %d\n", tidSet.size(), notFound);
+        Map<TermId, DirectAndIndirectTermAnnotations> studyAssociations = associationContainer.getAssociationMap(tidSet, ontology);
+
+        return new StudySet(tidSet, name, studyAssociations);
     }
 
 
--- src/main/java/org/jax/sbas2go/command/SbasCommand.java
+++ src/main/java/org/jax/sbas2go/command/SbasCommand.java
@@ -8,8 +8,15 @@ import org.jax.sbas2go.parsers.HgncParser;
 import org.jax.sbas2go.parsers.SbasParser;
 import org.monarchinitiative.phenol.analysis.AssociationContainer;
 import org.monarchinitiative.phenol.analysis.PopulationSet;
+import org.monarchinitiative.phenol.analysis.StudySet;
+import org.monarchinitiative.phenol.annotations.formats.go.GoGaf21Annotation;
 import org.monarchinitiative.phenol.ontology.data.Ontology;
+import org.monarchinitiative.phenol.ontology.data.Term;
 import org.monarchinitiative.phenol.ontology.data.TermId;
+import org.monarchinitiative.phenol.stats.GoTerm2PValAndCounts;
+import org.monarchinitiative.phenol.stats.TermForTermPValueCalculation;
+import org.monarchinitiative.phenol.stats.mtc.Bonferroni;
+import org.monarchinitiative.phenol.stats.mtc.MultipleTestingCorrection;
 import picocli.CommandLine;
 
 import java.util.List;
@@ -24,11 +31,15 @@ public class SbasCommand implements Callable<Integer> {
     @CommandLine.Option(names={"-s","--sbas"}, description ="directory to sbas data" )
     private String sbasdir;
 
+
+    private final double ALPHA = 0.05;
+
     @Override
     public Integer call() throws Exception {
         GoParser goParser = new GoParser("data");
         AssociationContainer associationContainer = goParser.getAssociationContainer();
         Ontology ontology = goParser.getOntology();
+        List<GoGaf21Annotation> annots =  goParser.getGoAnnotations();
         SbasParser sbasParser = new SbasParser(sbasdir);
         // the following has ENSEMBL ids
         Set<String> dgePopulation = sbasParser.getEnsembleDgePopulation();
@@ -43,11 +54,71 @@ public class SbasCommand implements Callable<Integer> {
                 ensembl2termIdMap,
                 symbol2termIdMap,
                 associationContainer,
+                annots,
                 ontology);
         PopulationSet populationSet = geneSetExtractor.getPopulationSet(dgePopulation, asGeneSymbols);
         System.out.printf("[INFO] Got populaton set with %d  annotated genes.\n", populationSet.getAnnotatedItemCount());
-
+        // do DGE analysis
+        for (var entry : dgeMap.entrySet()) {
+            String tissue = entry.getKey();
+            Set<Dge> dgeSet = entry.getValue();
+            StudySet study = geneSetExtractor.getStudySet(dgeSet, tissue);
+            doDgeAnalysis(tissue, study, ontology, associationContainer, populationSet);
+        }
+        //associationContainer.
 
         return 0;
     }
+
+
+    private void doDgeAnalysis(String tissue,
+                               StudySet study,
+                               Ontology ontology,
+                               AssociationContainer associationContainer,
+                               PopulationSet populationSet) {
+        System.out.println();
+        System.out.println("[INFO] DGE: Term-for-term analysis");
+        System.out.println();
+
+        MultipleTestingCorrection bonf = new Bonferroni();
+        TermForTermPValueCalculation tftpvalcal = new TermForTermPValueCalculation(ontology,
+                associationContainer,
+                populationSet,
+                study,
+                bonf);
+        List<GoTerm2PValAndCounts> pvals = tftpvalcal.calculatePVals();
+        System.out.println("[INFO] Total number of retrieved p values: " + pvals.size());
+        int n_sig = 0;
+        int studysize = study.getAnnotatedItemCount();
+        int popsize = populationSet.getAnnotatedItemCount();
+        System.out.println(String.format("[INFO] Study set: %d genes. Population set: %d genes",
+                studysize, popsize));
+        for (GoTerm2PValAndCounts item : pvals) {
+            double pval = item.getRawPValue();
+            double pval_adj = item.getAdjustedPValue();
+            TermId tid = item.getItem();
+            Term term = ontology.getTermMap().get(tid);
+            if (term == null) {
+                System.err.println("[ERROR] Could not retrieve term for " + tid.getValue());
+                continue;
+            }
+            String label = term.getName();
+            if (pval_adj > ALPHA) {
+                continue;
+            }
+            n_sig++;
+            double studypercentage = 100.0 * (double) item.getAnnotatedStudyGenes() / studysize;
+            double poppercentage = 100.0 * (double) item.getAnnotatedPopulationGenes() / popsize;
+            System.out.println(String.format("%s [%s]: %.2e (adjusted %.2e). Study: n=%d (%.1f%%); population: N=%d (%.1f%%)",
+                    label, tid.getValue(), pval, pval_adj, item.getAnnotatedStudyGenes(), studypercentage,
+                    item.getAnnotatedPopulationGenes(), poppercentage));
+        }
+        System.out.println(String.format("%d of %d terms were significant at alpha %.7f", n_sig, pvals.size(), ALPHA));
+    }
+
+
+
+
+
+
 }
